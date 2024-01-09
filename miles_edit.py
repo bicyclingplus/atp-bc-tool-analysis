d384038e3db8152e4953c2c4789d4823b4b84876
mRaffill <manonmani@damajash.org>
2024-01-09 14:25:30 -0800

    Changes to include the adjacent units adjustment factor Also added some print statements to help me figure out what is going on

--- miles_edit.py
+++ miles_edit.py
@@ -1,7 +1,7 @@
 import json
 import math
 
-def calculate_distance(proj_distance, proj_intersections, proj_volume):
+def calculate_distance(mode, proj_distance, proj_intersections, proj_volume, proj_connected):
     # -----------------------------------------------------------
     # function to estimate distance walked in a project
     #
@@ -16,10 +16,10 @@ def calculate_distance(proj_distance, proj_intersections, proj_volume):
     # distance: Total distance walked within the project for the given intersection volume
     # -----------------------------------------------------------
 
-    # Get data from the lookup table in config.json
-    f = open('config.json')
+    # Get data from the lookup table in config.json **EDIT: use the volume_to_miles instead, to test bicycling as well**
+    f = open('volume_to_miles.json')
     config_data = json.load(f)
-    miles_distribution = config_data['miles_walked'];
+    miles_distribution = config_data[mode]
 
     # Calculating the average distance per intersection for the project
     proj_distance_per_intersection = proj_distance/proj_intersections
@@ -29,15 +29,27 @@ def calculate_distance(proj_distance, proj_intersections, proj_volume):
     distribution_den = 0
     for dist in miles_distribution:
         intersection_distribution[dist] = math.floor(float(dist)/proj_distance_per_intersection)
+        print("miles distribution is",miles_distribution[dist])
+        print("intersection distribution original is",intersection_distribution[dist])
 
         # If on average people walk more than the number of intersections in the project, then consider they have walked through all of the project intersections
         if intersection_distribution[dist] > proj_intersections:
             intersection_distribution[dist] = proj_intersections
+        print("intersection distribution cut-off is",intersection_distribution[dist])
 
         # Distribution of people walking through intersections
-        distribution_den += intersection_distribution[dist]*miles_distribution[dist]
+        distribution_den += intersection_distribution[dist]*miles_distribution[dist] # number of intersections * % of people walking through them - weighted average # of intersections/ways each person is counted at
+        adjusted_den = (distribution_den * proj_connected) # people staying in the project (proj_connected): travel the average.
+        adjusted_den += (1-proj_connected)# People crossing (everyone else, 1 - proj_connected) travel 1 intersection.
+
+    print("average intersections traveled is",distribution_den)
+    print("adjusted average intersections traveled is",adjusted_den)
+    print(adjusted_den/distribution_den)
 
     people = math.floor(proj_volume/distribution_den)
+    people_adj = proj_volume/adjusted_den # total people/average intersections = unique people, not counted multiple times
+    print("unique people is",people)
+    print("adjusted unique people is",people_adj)
 
     # Calculating the distance walked in the project
     distance = 0
