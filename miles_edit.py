6adfd13f99e3fd2fb77618a15d55892fef692450
mRaffill <manonmani@damajash.org>
2024-01-23 20:14:59 -0800

    Temporarily skip the adjacent ways/intersections factor Until I can get data for all of the projects

--- miles_edit.py
+++ miles_edit.py
@@ -1,7 +1,7 @@
 import json
 import math
 
-def calculate_distance(mode, proj_distance, proj_intersections, proj_volume, proj_connected):
+def calculate_distance(mode, proj_distance, proj_intersections, proj_volume, proj_connected=-1):
     # -----------------------------------------------------------
     # function to estimate distance walked in a project
     #
@@ -23,28 +23,33 @@ def calculate_distance(mode, proj_distance, proj_intersections, proj_volume, pro
 
     # Calculating the average distance per intersection for the project
     proj_distance_per_intersection = proj_distance/proj_intersections
+    print("distance per intersection",proj_distance_per_intersection)
 
     # Convert miles into intersections
     intersection_distribution = {}
     distribution_den = 0
     for dist in miles_distribution:
         intersection_distribution[dist] = float(dist)/proj_distance_per_intersection
-        print("miles distribution is",dist)
-        print("intersection distribution is",intersection_distribution[dist])
+        # print("miles distribution is",dist)
+        # print("intersection distribution is",intersection_distribution[dist])
 
         # If on average people walk more than the number of intersections in the project, then consider they have walked through all of the project intersections
         if intersection_distribution[dist] > proj_intersections:
             intersection_distribution[dist] = proj_intersections
 
         # Distribution of people walking through intersections
+        #print(intersection_distribution[dist],miles_distribution[dist])
         distribution_den += intersection_distribution[dist]*miles_distribution[dist] # number of intersections * % of people walking through them - weighted average # of intersections/ways each person is counted at
+    if proj_connected != -1:
         adjusted_den = (distribution_den * proj_connected) # people staying in the project (proj_connected): travel the average.
-        print(adjusted_den)
         adjusted_den += (1-proj_connected)# People crossing (everyone else, 1 - proj_connected) travel 1 intersection.
-        print(adjusted_den)
-    print("average intersections traveled is",distribution_den)
-    print("adjusted average intersections traveled is",adjusted_den)
-    print("ratio is", adjusted_den/distribution_den)
+    else:
+        adjusted_den = distribution_den
+    print("intersections traveled", adjusted_den)
+
+    # print("average intersections traveled is",distribution_den)
+    # print("adjusted average intersections traveled is",adjusted_den)
+    # print("ratio is", adjusted_den/distribution_den)
 
     people = proj_volume/distribution_den
     people_adj = proj_volume/adjusted_den # total people/average intersections = unique people, not counted multiple times
@@ -56,9 +61,9 @@ def calculate_distance(mode, proj_distance, proj_intersections, proj_volume, pro
     distance = 0
     for dist in miles_distribution:
         if (float(dist)>proj_distance):
-            distance += proj_distance*miles_distribution[dist]*people
+            distance += proj_distance*miles_distribution[dist]*people_adj
         else:
-            distance += float(dist)*miles_distribution[dist]*people
+            distance += float(dist)*miles_distribution[dist]*people_adj
 
     distance = round(distance, 2)
 
